74e4d24a6a7bab03f7e2fd87eb4498b7d91ab1cf
BrenoGdS <breno.eletronica@gmail.com>
2025-04-06 20:49:25 -0300

    Configuring Error Handling for the Kafka Consumer.

--- src/main/java/com/appsdeveloperblog/ws/emailnotificationmicroservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotificationmicroservice/config/KafkaConsumerConfig.java
@@ -2,11 +2,13 @@ package com.appsdeveloperblog.ws.emailnotificationmicroservice.config;
 
 import com.appsdeveloperblog.ws.coreblog.event.ProductCreatedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -35,8 +37,10 @@ public class KafkaConsumerConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // Configure consumer to use an error-handling deserializer for message values
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class); // Wraps the actual deserializer to catch errors
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class); // Uses JsonDeserializer for converting bytes to JSON
         props.put(JsonDeserializer.TRUSTED_PACKAGES, trustedPackages);
 
         return props;
